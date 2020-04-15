19fdefabeb1df7415b0ca999fd8cd999583eaf12
Stefano <stefanobianchi9@gmail.com>
2020-04-15 21:59:43 +0200

    book escape button

--- app/src/main/java/com/stapp/trovaescape/details/EscapeDetails.java
+++ app/src/main/java/com/stapp/trovaescape/details/EscapeDetails.java
@@ -6,6 +6,7 @@ import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
--- app/src/main/java/com/stapp/trovaescape/details/RoomListAdapter.java
+++ app/src/main/java/com/stapp/trovaescape/details/RoomListAdapter.java
@@ -1,9 +1,12 @@
 package com.stapp.trovaescape.details;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,11 +27,13 @@ public class RoomListAdapter extends RecyclerView.Adapter<RoomListAdapter.ViewHo
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView name, prices, avails;
+        public Button book;
         public ViewHolder(View v) {
             super(v);
             name = v.findViewById(R.id.room_name);
             prices = v.findViewById(R.id.room_prices);
             avails = v.findViewById(R.id.room_avails);
+            book = v.findViewById(R.id.book_btn);
         }
     }
 
@@ -50,13 +55,25 @@ public class RoomListAdapter extends RecyclerView.Adapter<RoomListAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(final RoomListAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(final RoomListAdapter.ViewHolder holder, final int position) {
         holder.itemView.setOnClickListener(clickListener);
         if(roomList.size() > 0){
             holder.name.setText(roomList.get(position).getName());
             holder.prices.setText(Utils.getPricesFormatted(roomList.get(position).getPrices()));
             holder.avails.setText(Utils.getAvailsFormatted(roomList.get(position).getAvailabilities()));
+            holder.book.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    goToSite(roomList.get(position).getWebsite());
+                }
+            });
         }
+    }
+
+    private void goToSite(String url){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        ctx.startActivity(i);
     }
 
     @Override
